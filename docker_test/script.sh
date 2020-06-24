ec15e6780dfe5623c0d49df1719419591199eaf5
Lisa Vlamings <livlamin@f1r5s20.codam.nl>
2020-06-24 16:45:54 +0200

    ft_server

--- docker_test/script.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-#rename nginx conf-file
-#mv nginx.conf nginx.conf.ORG
-#create a new nginx.config file
-nano docker-compose.yml
-#nano nginx.conf
-#top
---
+++ docker_test/srcs/script.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+service nginx start
+# service mariadb-server start
+# service php start
+/bin/bash
+tail -f/dev/null
+
+
+#rename nginx conf-file
+#mv nginx.conf nginx.conf.ORG
+#create a new nginx.config file

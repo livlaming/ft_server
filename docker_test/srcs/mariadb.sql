703c4624bc917545808f955af62248def90b39b2
lisa vlamings <lisavlamings@MacBook-Pro-van-lisa-2.local>
2020-06-29 22:04:58 +0200

    ft_server

--- docker_test/srcs/mariadb.sql
+++ docker_test/srcs/mariadb.sql
@@ -1,8 +1,12 @@
-CREATE DATABASE wordpress;
+-- mariadb?
+-- CREATE DATABASE wordpress;
 CREATE DATABASE phpmyadmin;
-CREATE USER 'lisa'@'localhost' identified by 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'localhost';
+GRANT ALL ON phpmyadmin.* TO 'lisa'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+-- CREATE USER 'lisa'@'localhost' identified by 'password';
+-- GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'localhost';
+
+
 -- GRANT ALL ON *.* TO 'phpmyadmin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
 -- CREATE DATABASE wordpress;
 -- GRANT ALL ON wordpress.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;

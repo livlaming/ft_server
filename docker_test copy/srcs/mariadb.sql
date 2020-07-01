7a492f6ab889726ff5a82fdec021911b46d2e5db
Lisa Vlamings <livlamin@f1r5s21.codam.nl>
2020-07-01 12:30:06 +0200

    ft_ser

---
+++ docker_test copy/srcs/mariadb.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE wordpress;
+CREATE DATABASE phpmyadmin;
+CREATE USER 'lisa'@'localhost' identified by 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'localhost';
+FLUSH PRIVILEGES;
+-- GRANT ALL ON *.* TO 'phpmyadmin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
+-- CREATE DATABASE wordpress;
+-- GRANT ALL ON wordpress.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
+
+-- SET PASSWORD FOR 'example_user'@localhost = PASSWORD("");

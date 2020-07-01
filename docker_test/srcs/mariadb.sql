4a2e3baad2809033e0b0ed58c225f55c3e498a4e
Lisa Vlamings <livlamin@f1r5s21.codam.nl>
2020-07-01 13:18:38 +0200

    ft-server

--- docker_test/srcs/mariadb.sql
+++ docker_test/srcs/mariadb.sql
@@ -1,14 +1,5 @@
--- mariadb?
--- CREATE DATABASE wordpress;
+CREATE DATABASE wordpress;
 CREATE DATABASE phpmyadmin;
-GRANT ALL ON phpmyadmin.* TO 'lisa'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
+CREATE USER 'lisa'@'localhost' identified by 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'localhost';
 FLUSH PRIVILEGES;
--- CREATE USER 'lisa'@'localhost' identified by 'password';
--- GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'localhost';
-
-
--- GRANT ALL ON *.* TO 'phpmyadmin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
--- CREATE DATABASE wordpress;
--- GRANT ALL ON wordpress.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
-
--- SET PASSWORD FOR 'example_user'@localhost = PASSWORD("");

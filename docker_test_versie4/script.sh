e4b1e21be86c2303a01910810b65568739ecd677
Lisa Vlamings <livlamin@f1r4s18.codam.nl>
2020-07-03 16:44:34 +0200

    ft

---
+++ docker_test_versie4/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+service nginx start
+# service mysql start
+# service php start  
+# /bin/bash
+# tail -f/dev/null
+
+#waarom bin/bash?
+
+#rename nginx conf-file
+#mv nginx.conf nginx.conf.ORG
+#create a new nginx.config file

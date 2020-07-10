64b02d7391edf4124397ad36efd93bd450339b40
Lisa Vlamings <livlamin@f1r4s11.codam.nl>
2020-07-10 17:23:32 +0200

    ft_server klaaarrr??

--- inleveren/srcs/set_autoindex.sh
+++ inleveren/srcs/set_autoindex.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 if [ "$1" == "off" ]
+then
     sed -i 's/autoindex on/autoindex off/' /etc/nginx/sites-available/default && \
     service nginx restart
-else if [ "$1" == "on" ]
-    sed -i 's/autoindex off /autoindex on/' /etc/nginx/sites-available/default && \
+elif [ "$1" == "on" ]
+then
+    sed -i 's/autoindex off/autoindex on/' /etc/nginx/sites-available/default && \
     service nginx restart
 fi

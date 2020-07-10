13cf018718b7e8bf78082779f22980c4a87fc14c
lisa vlamings <lisavlamings@MacBook-Pro-van-lisa-2.local>
2020-07-10 10:59:02 +0200

    docker mappen schoon

--- docker_test_versie2/srcs/mariadb.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE DATABASE phpmyadmin;
-CREATE USER 'lisa'@'localhost' identified by 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'localhost';
-FLUSH PRIVILEGES;

b60dac60bc83ba527fe306715e067af9d282b662
Lisa Vlamings <livlamin@f1r4s8.codam.nl>
2020-07-13 15:10:31 +0200

    herschikking

--- inleveren_2/srcs/mariadb.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'lisa'@'localhost' identified by 'password';
-CREATE DATABASE wordpress;
-CREATE DATABASE phpmyadmin;
-GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'localhost' IDENTIFIED BY 'password';
-FLUSH PRIVILEGES;
